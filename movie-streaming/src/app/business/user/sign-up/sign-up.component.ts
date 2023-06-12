47db6c022a25ee4cb8d289fadc87f8090205b6ae
JohnnySmith <swelwlpmp@gmail.com>
2023-06-12 13:11:20 +0630

    signUp

--- movie-streaming/src/app/business/user/sign-up/sign-up.component.ts
+++ movie-streaming/src/app/business/user/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent {
+
+  signupForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.signupForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    });
+  }
+
+  get form() { return this.signupForm.controls; }
+
+  onSubmit() {
+    if (this.signupForm.invalid) {
+      return;
+    }
+
+    console.log(this.signupForm.value);
+  }
 
 }
