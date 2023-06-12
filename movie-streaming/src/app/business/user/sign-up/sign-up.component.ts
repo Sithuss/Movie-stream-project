a28220431160ff3237357f5245d160addfc937c3
Sithuss <pla243331@gmail.com>
2023-06-12 23:19:30 +0630

    signUp form for uploader

--- movie-streaming/src/app/business/user/sign-up/sign-up.component.ts
+++ movie-streaming/src/app/business/user/sign-up/sign-up.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { SecurityService } from 'src/app/service/security/security.service';
+import { passwordMatchValidator } from 'src/app/service/validators/password.match';
+
 
 @Component({
   selector: 'app-sign-up',
@@ -10,18 +11,36 @@ import { SecurityService } from 'src/app/service/security/security.service';
 })
 export class SignUpComponent {
 
+  uploader=false;
+
+  status:string = "uploader"
+
   signupForm: FormGroup;
 
-  constructor(private fb:FormBuilder) {
+  constructor(private fb:FormBuilder, private router:Router) {
     this.signupForm = fb.group({
       name:['',[Validators.required]],
       email:['', [Validators.required, Validators.email]],
-      password:['', [Validators.required]]
+      password:['', [Validators.required]],
+      // confirmPassword:['', [passwordMatchValidator()]],
+      transaction:['', [Validators.required]]
     })
   }
 
   SignUp() {
     console.log(this.signupForm.value);
+    this.router.navigate(['/user/sign-in']);
+  }
+
+  changeUploader() {
+    if(this.uploader === false) {
+      this.uploader = true;
+      this.status = "user";
+    } else {
+      this.signupForm.reset();
+      this.uploader = false;
+      this.status = "uploader"
+    }
   }
 
 }
