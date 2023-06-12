e6a539e701053acf6fd9437539589b5a89d21f0f
waiyanmm <waiyan.f3lix@gmail.com>
2023-06-12 14:28:17 +0630

    uploader

--- movie-streaming/src/app/business/uploader/history-list/history-list.component.ts
+++ movie-streaming/src/app/business/uploader/history-list/history-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, Input, OnInit } from '@angular/core';
 import { DayNightService } from 'src/app/service/day-night.service';
 import { MovieService } from 'src/app/service/movie.service';
 
@@ -7,17 +7,21 @@ import { MovieService } from 'src/app/service/movie.service';
   templateUrl: './history-list.component.html',
   styleUrls: ['./history-list.component.css']
 })
-export class HistoryListComponent implements OnInit{
+export class HistoryListComponent implements OnInit, AfterViewChecked{
 
   constructor(public dayNightService: DayNightService,
     private movieServie: MovieService){
-      this.isNight = this.dayNightService.isNight().nightMode;
+
     }
 
-  isNight!:boolean;
+  isNight:boolean = true;
 
   movieList:any[] = []
   ngOnInit(): void {
     this.movieServie.findAll().subscribe(r => {this.movieList = r})
+  }
+
+  ngAfterViewChecked(): void {
+    this.isNight = !this.dayNightService.isNight().nightMode;
   }
 }
