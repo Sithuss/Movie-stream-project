4595621e2b40c0c01cb3caf55f039299ac9f3299
Your Name <eiphyu6147@gmail.com>
2023-06-12 15:47:46 +0630

    signin

--- movie-streaming/src/app/business/user/user.module.ts
+++ movie-streaming/src/app/business/user/user.module.ts
@@ -13,6 +13,7 @@ import { EditAccountComponent } from 'src/app/business/user/edit-account/edit-ac
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { WidgetsModule } from 'src/app/common/widgets/widgets.module';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,6 @@ import { SignInComponent } from './sign-in/sign-in.component';
     UserHomeComponent,
     MovieDetailsComponent,
     GiveReviewsComponent,
-    UserAccountComponent,
     EditAccountComponent,
     SignUpComponent,
     SignInComponent
@@ -31,7 +31,8 @@ import { SignInComponent } from './sign-in/sign-in.component';
     CommonModule,
     UserRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    WidgetsModule
   ]
 })
 export class UserModule { }
