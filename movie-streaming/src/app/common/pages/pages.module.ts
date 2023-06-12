353843de1d80aed5141bc8e93ebc19f537ef3ec2
Sithuss <pla243331@gmail.com>
2023-06-12 11:31:55 +0630

    ready to pull

--- movie-streaming/src/app/common/pages/pages.module.ts
+++ movie-streaming/src/app/common/pages/pages.module.ts
@@ -1,11 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserAccountComponent } from './user-account/user-account.component';
-import { EditAccountComponent } from '../../business/user/edit-account/edit-account.component';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { UserComponent } from 'src/app/business/user/user.component';
-
 
 @NgModule({
   declarations: [
