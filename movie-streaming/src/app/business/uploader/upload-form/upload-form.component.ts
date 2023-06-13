dbce688ee34e37e6d8ff4b327223228a1c52c5a8
KyawHtet8 <nto4777@gmail.com>
2023-06-13 11:28:20 +0630

    Fix Conflict

--- movie-streaming/src/app/business/uploader/upload-form/upload-form.component.ts
+++ movie-streaming/src/app/business/uploader/upload-form/upload-form.component.ts
@@ -1,11 +1,8 @@
 import { Movie } from './../model/movie';
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-=======
 import { MovieService } from 'src/app/service/movie.service';
->>>>>>> kh-dev
 
 @Component({
   selector: 'app-upload-form',
@@ -13,32 +10,20 @@ import { MovieService } from 'src/app/service/movie.service';
 })
 export class UploadFormComponent {
 
-<<<<<<< HEAD
   form:FormGroup
 
   constructor(
     builder:FormBuilder,
-
+    private mService:MovieService,
     private router:Router){
       this.form = builder.group({
         title:['',Validators.required],
         category:['',Validators.required]
-
-
-
-
-
       })
     }
 
 
-=======
-  constructor(private mService:MovieService){
-
-  }
-
   uploadM(m:Movie){
     this.mService.upload(m);
   }
->>>>>>> kh-dev
 }
