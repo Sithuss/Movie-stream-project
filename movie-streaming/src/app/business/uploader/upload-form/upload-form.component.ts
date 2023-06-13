56ca09dfda2b2e7df06bbeb877d94d39a6ad6dc7
waiyanmm <waiyan.f3lix@gmail.com>
2023-06-13 14:48:42 +0630

    uploader

--- movie-streaming/src/app/business/uploader/upload-form/upload-form.component.ts
+++ movie-streaming/src/app/business/uploader/upload-form/upload-form.component.ts
@@ -1,6 +1,7 @@
 import { Movie } from './../model/movie';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { MovieService } from 'src/app/service/movie.service';
 
 @Component({
@@ -12,10 +13,10 @@ export class UploadFormComponent {
 
   form:FormGroup
 
-  constructor(private mService:MovieService, builder:FormBuilder){
+  constructor(private mService:MovieService, builder:FormBuilder, private router:Router){
     this.form = builder.group({
       title: ['', [Validators.required]],
-      length: ['', [Validators.required, Validators.min(2)]],
+      length: ['', [Validators.required]],
       category: ['', [Validators.required]],
       actor: ['', [Validators.required]],
       actress: ['', [Validators.required]],
@@ -31,5 +32,6 @@ export class UploadFormComponent {
 
   uploadM(m:Movie){
     this.mService.upload(m);
+    this.router.navigate(['/uploader', 'home'])
   }
 }
