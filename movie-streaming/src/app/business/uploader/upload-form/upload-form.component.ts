ddee054f95fc3b2a4203aeac7f21fd72763b1af9
KyawHtet8 <nto4777@gmail.com>
2023-06-13 10:30:48 +0630

    form

--- movie-streaming/src/app/business/uploader/upload-form/upload-form.component.ts
+++ movie-streaming/src/app/business/uploader/upload-form/upload-form.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-upload-form',
   templateUrl: './upload-form.component.html'
 })
 export class UploadFormComponent {
+
+  form:FormGroup
+
+  constructor(
+    builder:FormBuilder,
+
+    private router:Router){
+      this.form = builder.group({
+        title:['',Validators.required],
+        category:['',Validators.required]
+
+
+
+
+
+      })
+    }
+
 
 }
