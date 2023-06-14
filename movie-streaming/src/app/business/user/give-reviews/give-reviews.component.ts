45845c281ac12d351dc38b7651cdac743d0a8e2c
Yse777 <shweyoon439@gmail.com>
2023-06-14 16:01:07 +0630

    review page

--- movie-streaming/src/app/business/user/give-reviews/give-reviews.component.ts
+++ movie-streaming/src/app/business/user/give-reviews/give-reviews.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Review } from 'src/app/service/dto/review';
+import { ReviewPageService } from 'src/app/service/review-page.service';
+import { Movie } from '../../uploader/model/movie';
 
 @Component({
   selector: 'app-give-reviews',
   templateUrl: './give-reviews.component.html',
   styleUrls: ['./give-reviews.component.css']
 })
-export class GiveReviewsComponent {
+export class GiveReviewsComponent implements OnInit{
+
+  reviewForm: FormGroup;
+
+  otherReview!:Observable<Review>;
+
+  reviewList: BehaviorSubject<Review []> = new BehaviorSubject<Review []>([]);
+  reviewList$: Observable<Review[]> = this.reviewList.asObservable();
+
+  constructor(private fb:FormBuilder,private router:Router, private service:ReviewPageService) {
+    this.reviewForm= fb.group( {
+      id:3,
+      review : ""
+    }
+
+    )
+  }
+  ngOnInit(): void {
+    this.service.findAllReviews().subscribe(m => { this.reviewList.next(m)});
+  }
+
+  addReview() {
+
+    this.service.createReview(this.reviewForm.value);
+    // console.log(this.signupForm.value);
+    //TODO
+    this.router.navigate(['/user/give-reviews']);
+  }
 
 }
---
+++ movie-streaming/src/app/service/review-page.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Review } from './dto/review';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'any',
+})
+export class ReviewPageService {
+
+
+
+  constructor(){}
+
+  private formList:Review[] = [
+    {id: 1, review:"This site is not bad.I hope more latest movies."},
+    {id: 2, review:"I waiting for new episodes."}
+  ]
+
+  findAllReviews():Observable<Review[]> {
+    return of(this.formList);
+  }
+
+  createReview(reviewForm:Review){
+    this.formList.push(reviewForm);
+  }
+}
