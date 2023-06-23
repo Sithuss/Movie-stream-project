2dcf3e8343adc90b01fc8e4182a11581d1f053bc
Sithuss <pla243331@gmail.com>
2023-06-23 14:09:06 +0630

    added sec dep added

--- movie-streaming-api/src/main/java/com/streaming/team3/api/publicApi/PublicSecurityApi.java
+++ movie-streaming-api/src/main/java/com/streaming/team3/api/publicApi/PublicSecurityApi.java
@@ -1,6 +1,8 @@
 package com.streaming.team3.api.publicApi;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.streaming.team3.domain.dto.ApiResult;
 import com.streaming.team3.domain.dto.SignInDto;
 import com.streaming.team3.domain.dto.UploaderSignUpDto;
+import com.streaming.team3.domain.dto.VO.LoginUserVO;
 import com.streaming.team3.domain.dto.form.UserSignUpForm;
 import com.streaming.team3.domain.service.SecurityService;
 
@@ -28,9 +31,9 @@ public class PublicSecurityApi {
         return null;
     }
 
-    @PostMapping("/security/regiester")
-    public ApiResult userSignUp(@RequestBody UserSignUpForm form) {
-        // TODO implement here
+    @PostMapping("/security/register")
+    public ApiResult<LoginUserVO> userSignUp(@RequestBody @Validated UserSignUpForm form, BindingResult result) {
+        
         return null;
     }
 
